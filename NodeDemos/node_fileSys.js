b0ff2fbc4bee9efaa5e5318b307917963b1b4e59
Tharaka Herath <tashkraze@gmail.com>
2016-08-31 19:34:38 +0530

    node fileSys partially done

--- NodeDemos/node_fileSys.js
+++ NodeDemos/node_fileSys.js
@@ -51,6 +51,70 @@ fs.writeFile('input.txt','new content from script', function(err){
     });
 });*/
 
+// Reading a file
+
+/*var fs = require('fs');
+var buf = new Buffer(1024);
+
+console.log('Going to open an existing file');
+
+fs.open('input.txt', 'r+', function(err, fd){
+    if(err){
+        console.log(err);
+    }
+    console.log('File Opened Successfully');
+    console.log('Truncate starting....');
+
+    fs.ftruncate(fd, 10, function(err){
+      if (err){
+         console.log(err);
+      } 
+      console.log("File truncated successfully.");
+      console.log('Going to read the file');
+      fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+        if (err){
+          console.log(err);
+       }
+       console.log(bytes + " bytes read");
+      // Print only read bytes to avoid junk.
+       if(bytes > 0){
+          console.log(buf.slice(0, bytes).toString());
+       }
+
+        fs.close(fd, function(err){
+          if(err){
+             console.log(err);
+          }
+          console.log('File closed successfully');
+
+          fs.unlink('input.txt',function(err){
+              if(err){
+                  return console.log(err);
+              }
+              console.log("File deleted successfully!");
+          });
+
+        });
+      });
+    });
+});*/
+
+// Create Directory
+var fs = require('fs');
+console.log("Going to create directory /tmp/test");
+fs.mkdir('tmp/test',function(err){
+    if(err){
+        return console.log(err);
+    }
+    console.log("Directory created successfully!");
+});
+
+
+
+
+
+
+
 
 
 
