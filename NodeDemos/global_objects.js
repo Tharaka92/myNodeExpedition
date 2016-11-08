15592b36f3f8ae4f23ad69dd6f6c7f3f62f5cc41
Tharaka Herath <tashkraze@gmail.com>
2016-11-08 19:31:22 +0530

    global objects tute done.

--- NodeDemos/global_objects.js
+++ NodeDemos/global_objects.js
@@ -1,1 +1,12 @@
-console.log( __filename );
+//console.log( "this file name : " + __filename );
+//console.log( "this directory name : " + __dirname );
+
+function hello(){
+    console.log("Hello Tharaka!");
+}
+
+//var result = setTimeout(hello, 2000);
+//clearTimeout(result);
+
+var result = setInterval(hello,2000);
+clearInterval(result);
